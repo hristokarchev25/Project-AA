425f06d36f7b2b7674db97b4dd27c70dc211682c
Hristo Karchev <x_karchev@mail.bg>
2021-03-26 12:26:23 +0200

    Router working + small test added //toBe deleted

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,26 @@
+import { Route, Switch } from 'react-router-dom';
+
+
+
 import style from './App.module.css';
 import Header from './components/Header/Header';
 import Footer from './components/Footer/Footer';
 import About from './components/About/About'
 
+
+import Demo from './components/Demo/Demo';
 function App() {
   return (
     <div className={style.app}>
       <Header />
       <div className={style.container}>
 
-        <About />
-       
-        
+        <Switch>
+           <Route path="/" exact component={About}/>
+           <Route path="/pathTest"  component={Demo}/>
+        </Switch>
+
+
       </div>
       <Footer />
     </div>
--- client/src/components/Header/Header.js
+++ client/src/components/Header/Header.js
@@ -1,3 +1,4 @@
+import { Link } from 'react-router-dom';
 import style from './Header.module.css';
 
 const Header = () => {
@@ -5,8 +6,8 @@ const Header = () => {
         <nav className={style.navigation}>
             <ul>
                 <li className={style.listItem}><img className={style.logo} src="/App.png" alt="app png" /> </li>
-                <li className={style.listItem}><a className={style.navListItem} href="#">About Us</a></li>
-                <li className={style.listItem}><a className={style.navListItem} href="#">Path 2</a></li>
+                <li className={style.listItem}><Link className={style.navListItem} to="/">About Us</Link></li>
+                <li className={style.listItem}><Link className={style.navListItem} to="/pathTest">Path 2</Link></li>
                 <li className={style.listItem}><a className={style.navListItem} href="#">Path 3</a></li>
 
             </ul>
