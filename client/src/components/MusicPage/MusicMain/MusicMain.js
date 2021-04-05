9082b948fab04fc2ed28f224596d36c074160a3b
Hristo Karchev <x_karchev@mail.bg>
2021-04-05 12:54:57 +0300

    Geting the songs from the collection, finally WORKING

--- client/src/components/MusicPage/MusicMain/MusicMain.js
+++ client/src/components/MusicPage/MusicMain/MusicMain.js
@@ -1,62 +1,42 @@
+import { Component } from 'react';
+
 import style from './MusicMain.module.css';
 import { db } from '../../../utils/firebase';
-import { useState, useEffect } from 'react';
-
 import SongCard from './SongCard/SongCard';
 
-const MusicMain = () => {
-    const [songs, setSongs] = useState([])
-
-    const fetchSongs = async () => {
-        const response = db.collection('songs');
-        const data = await response.get();
-        data.docs.forEach(item => {
-            setSongs([...songs, item.data()])
-        })
+class MusicMain extends Component {
+    state = {
+        songs: null
     }
 
-    useEffect(() => {
-        fetchSongs();
-    }, [])
-
-    return (
-        <main className={style.container}>
-            <h1><img className={style.logo} src="/Music.png" alt="Music logo" /></h1>
-
-            {
-                songs && songs.map((song, index) => {
-                    return (
-                        /*  <div className={style.box}>
-                             <img className={style.box} src={`${song.cover}`} />
-                             <p className={style.name}>{song.song}</p>
-                             <p><span>By:</span>{song.artist}</p>
-                             <a className={style.btn} href="#">Details</a>
-                             <a className={style.btn} href="#">Details</a>
-                         </div> */
-                        <SongCard key={index}{...song} />
-                    )
+    componentDidMount() {
+        db.collection('songs')
+            .get()
+            .then(snapshot => {
+                const songs = [];
+                snapshot.forEach(doc => {
+                    const data = doc.data();
+                    songs.push(data);
                 })
-            }
+                this.setState({ songs: songs });
+            })
+            .catch(err => console.log(err));
+    }
 
-            {/*  <div className={style.box}>
-                <img className={style.box} src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRbIsG2bpZY03uuYJuQd0iZ46jgtabfR5IR6A&usqp=CAU" />
-                <p className={style.name}>CAAAAT</p>
-                <p><span>By:</span>Bigger Cat</p>
-                <a className={style.btn} href="#">Details</a>
-                <a className={style.btn} href="#">Details</a>
-            </div>
+    render() {
+        return (
+            <main className={style.container}>
+                <h1><img className={style.logo} src="/Music.png" alt="Music logo" /></h1>
 
-            <div className={style.box}>
-                <img className={style.box} src="https://static.stereogum.com/uploads/2020/07/image003-3-1595598757.jpg" />
-                <p className={style.name}>Wunna</p>
-                <p><span>By:</span>Gunna</p>
-                <a className={style.btn} href="#">Details</a>
-                <a className={style.btn} href="#">Details</a>
-            </div> */}
-
-
-        </main>
-    );
+                {   this.state.songs &&
+                    this.state.songs.map(song => {
+                        return (
+                            <SongCard {...song} />
+                        )
+                    })
+                }
+            </main>
+        );
+    }
 }
-
 export default MusicMain;
--- client/src/components/MusicPage/MusicMain/SongCard/SongCard.js
+++ client/src/components/MusicPage/MusicMain/SongCard/SongCard.js
@@ -2,7 +2,6 @@ import style from './SongCard.module.css';
 
 
 const SongCard = ({
-    id,
     cover,
     song,
     artist
@@ -15,7 +14,6 @@ const SongCard = ({
             <img className={style.box} src={`${cover}`} />
             <p className={style.name}>{song}</p>
             <p><span>By:</span>{artist}</p>
-            <a className={style.btn} href="#">Details</a>
             <a className={style.btn} href="#">Details</a>
         </div>
 
